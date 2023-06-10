a5910c7e03594dbe2883597a58732eb8e99d2518
EdKazik <edoskazik@gmail.com>
2023-06-10 20:09:01 +0200

    create if/else for form

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.querySelector('#button_buy').addEventListener('click', () => {
 const userForm = document.querySelector('#user__list-form');
 const orderBtn = document.querySelector('#button_order');
 const inputName = document.querySelector('#full_name');
+const inputPost = document.querySelector('#post')
+const inputQuantity =document.querySelector('#quantity')
 
 userForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -17,10 +19,11 @@ userForm.addEventListener('submit', (event) => {
 
     for (const value of formValues) {
         formObject[value[0]] = value[1];
-        
-        if (value[1].length < 1 ) {
+        console.log(formObject['full_name'])
+
+        if (formObject['full_name'] === '' || formObject['post'] === '' || formObject['quantity'] === '') {
+            alert('INPUTNAME')
             document.querySelector('.error__block').style.display = 'block';
-            alert('IF')
         } else {
             alert('ELSE')
             document.querySelector('.error__block').style.display = 'none';
@@ -30,7 +33,7 @@ userForm.addEventListener('submit', (event) => {
         
             const newSpan = document.createElement('span');
             newDiv.appendChild(newSpan)
-            newSpan.innerHTML = `${value[1]}`
+            newSpan.innerHTML = `${value[1]}`   
         }
     }
 });
