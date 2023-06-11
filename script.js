ac44b5147bf20a2f488dd1b600649b05b6b3fed7
EdKazik <edoskazik@gmail.com>
2023-06-11 17:45:59 +0200

    final commit

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ document.querySelector('#button_buy').addEventListener('click', () => {
 })
 
 const userForm = document.querySelector('#user__list-form');
-const orderBtn = document.querySelector('#button_order');
-const inputName = document.querySelector('#full_name');
-const inputPost = document.querySelector('#post')
-const inputQuantity =document.querySelector('#quantity')
+
 
 userForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -19,20 +16,20 @@ userForm.addEventListener('submit', (event) => {
 
     for (const value of formValues) {
         formObject[value[0]] = value[1];
-
+    }
         if (full_name.value.length === 0 || post.value.length === 0 || quantity.value.length === 0) {
             document.querySelector('.error__block').style.display = 'block';
         } else if (full_name.value.length > 0 || post.value.length > 0 || quantity.value.length > 0) {
             document.querySelector('.error__block').style.display = 'none';
+            
             const newDiv = document.createElement('div');
             newDiv.classList.add('new__div');
             document.querySelector('.container').appendChild(newDiv);
         
-            const newSpan = document.createElement('span');
-            newDiv.appendChild(newSpan)
-            newSpan.innerHTML = `${value[1]}`   
+            const newP = document.createElement('p');
+            newDiv.appendChild(newP);
+            newP.textContent = `Замовник: ${formObject.full_name}, Місто ${formObject.city} склад НП №${formObject.post}, ${formObject.quantity}шт`;
         }
-    }
 });
 
 
